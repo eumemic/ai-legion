a1b0a3c2e89eeccdc4027c6442be40a95d0f1458
eumemic <eumemic@gmail.com>
2023-04-10 13:57:49 -0700

    reintroduce openai delay

--- src/openai.ts
+++ src/openai.ts
@@ -4,23 +4,26 @@ import { Message } from "./message";
 import TaskQueue from "./task-queue";
 import { sleep } from "./util";
 
+const openaiDelay = 10 * 1000;
+
 const taskQueue = new TaskQueue();
 
 export default function generateText(agentId: string, messages: Message[]) {
-  return taskQueue.run(async () => {
+  const result = taskQueue.run(async () => {
     // console.log(`Agent ${agentId} reflecting on ${messages.length} messages`);
-    try {
-      const result = await openai().createChatCompletion({
-        model: "gpt-3.5-turbo",
-        // model: "gpt-4",
-        messages: messages.map((m) => m.openaiMessage),
-      });
-      // console.log(`Agent ${agentId} arrived at a response`);
-      return result;
-    } finally {
-      // await sleep(5000); // avoid rate limits
-    }
+    const result = await openai().createChatCompletion({
+      model: "gpt-3.5-turbo",
+      // model: "gpt-4",
+      messages: messages.map((m) => m.openaiMessage),
+    });
+    // console.log(`Agent ${agentId} arrived at a response`);
+    return result;
   });
+
+  // avoid rate limits
+  result.finally(() => taskQueue.run(() => sleep(openaiDelay)));
+
+  return result;
 }
 
 // lazy load to avoid accessing OPENAI_API_KEY before env has been loaded
