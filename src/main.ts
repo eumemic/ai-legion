cd55c31560ff52d126d9fb5f67ae253681829519
eumemic <eumemic@gmail.com>
2023-04-10 13:57:50 -0700

    get the agents to discuss amongst themselves

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ import { agentName } from "./util";
 
 dotenv.config();
 
-const numberOfAgents = 2;
+const numberOfAgents = 3;
 
 const agentIds = Array.from({ length: numberOfAgents }, (_, i) => `${i}`);
 
@@ -26,6 +26,10 @@ async function main() {
   for (const id of agentIds.slice(1)) {
     const memory: Memory = new InMemoryMemory(
       messageBuilder.primer(id),
+      messageBuilder.generic(
+        id,
+        `this is your first task. using the actions at your disposal, familiarize yourselves with the codebase. discuss with the other agent(s), comparing notes about how you think things work. once you feel like you understand it at a high level, one of you should write up a README.md file in the root directory summarizing the project.`
+      ),
       messageBuilder.agentResponse(id, "help"),
       messageBuilder.listAllActions(id)
     );
