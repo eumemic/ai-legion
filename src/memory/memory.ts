60d2bbfce38bde5f9c053aff854c4a2a3c14e80c
eumemic <eumemic@gmail.com>
2023-04-10 13:57:57 -0700

    insert a noop at the beginning so the agent waits to be given instructions

--- src/memory/memory.ts
+++ src/memory/memory.ts
@@ -39,7 +39,10 @@ export class Memory {
   async retrieve(): Promise<Event[]> {
     const introduction = await this.getIntroduction();
     const storedEvents = await this.store.get(this.key);
-    let events = [introduction, ...(storedEvents || [])];
+    let events = [
+      introduction,
+      ...(storedEvents || [{ type: "decision", actionText: "noop" }]),
+    ];
     if (this.firstRetrieval) {
       this.firstRetrieval = false;
       events = await this.summarize(events);
