f0d028f53461809fef05b9ce082626f7e880f0ce
eumemic <eumemic@gmail.com>
2023-04-10 13:57:53 -0700

    tell agent not to format summary as an action

--- src/memory/memory.ts
+++ src/memory/memory.ts
@@ -69,7 +69,7 @@ export class Memory {
               type: "message",
               message: messageBuilder.standard(
                 this.agentId,
-                `Write a summary in ${summaryWordLimit} words or less of what has happened since (but not including) the introductory message. Include key information that you learned which you don't want to forget. This information will serve as a note to yourself to help you understand what has gone before. Use the second person voice, as if you are someone filling in your replacement who knows nothing. The summarized messages will be omitted from your context window going forward and you will only have this summary to go by, so make it as useful and information-dense as possible. Be as specific as possible, but only include important information. If there are details that seem unimportant, or which you could recover outside of your memory (for instance the particular contents of a file which you could read any time), then omit them from your summary. Once again, your summary must not exceed ${summaryWordLimit} words.`
+                `Write a summary in ${summaryWordLimit} words or less of what has happened since (but not including) the introductory message. Include key information that you learned which you don't want to forget. This information will serve as a note to yourself to help you understand what has gone before. Use the second person voice, as if you are someone filling in your replacement who knows nothing. The summarized messages will be omitted from your context window going forward and you will only have this summary to go by, so make it as useful and information-dense as possible. Be as specific as possible, but only include important information. If there are details that seem unimportant, or which you could recover outside of your memory (for instance the particular contents of a file which you could read any time), then omit them from your summary. Once again, your summary must not exceed ${summaryWordLimit} words. In this particular instance, your response should just be raw text, not formatted as an action.`
               ),
             },
           ]);
@@ -82,7 +82,7 @@ export class Memory {
           const tokenSavings = precedingTokens - summaryTokens;
           if (tokenSavings > 0) {
             // console.log(
-            //   `Summarized ${summarizedEvents.length} events, saving ${tokenSavings} tokens:\n\n${summary}`
+            //   `Summarized events, saving ${tokenSavings} tokens:\n\n${summary}`
             // );
 
             return [events[0], summaryEvent, ...events.slice(i)];
