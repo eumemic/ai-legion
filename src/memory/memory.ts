823d1ee9c86cefd5a185696d9058baa1ee5d1bed
eumemic <eumemic@gmail.com>
2023-04-10 13:57:56 -0700

    fix bugs with memory summarizing/make the code even more hacky

--- src/memory/memory.ts
+++ src/memory/memory.ts
@@ -11,6 +11,8 @@ import {
 } from "../util";
 
 export class Memory {
+  private firstRetrieval = true;
+
   constructor(
     private agentId: string,
     private moduleManager: ModuleManager,
@@ -37,9 +39,14 @@ export class Memory {
   async retrieve(): Promise<Event[]> {
     const introduction = await this.getIntroduction();
     const storedEvents = await this.store.get(this.key);
-    const allEvents = [introduction, ...(storedEvents || [])];
-    // allEvents.forEach((event) => this.printEvent(event));
-    return allEvents;
+    let events = [introduction, ...(storedEvents || [])];
+    if (this.firstRetrieval) {
+      this.firstRetrieval = false;
+      events = await this.summarize(events);
+      await this.store.set(this.key, events.slice(1));
+    }
+    // events.forEach((event) => this.printEvent(event));
+    return events;
   }
 
   private async getIntroduction(): Promise<Event> {
@@ -91,11 +98,7 @@ export class Memory {
   private async summarize(events: Event[]): Promise<Event[]> {
     if (!events.length) return [];
 
-    const cumulativeTokenCounts = events.reduce((counts, event) => {
-      const prevSum = counts.length ? counts[counts.length - 1] : 0;
-      counts.push(prevSum + countTokens(event));
-      return counts;
-    }, [] as number[]);
+    const cumulativeTokenCounts = this.getCumulativeTokenCounts(events);
 
     const totalTokenCount =
       cumulativeTokenCounts[cumulativeTokenCounts.length - 1];
@@ -107,53 +110,84 @@ export class Memory {
         Math.floor((totalTokenCount - cumulativeTokenCounts[0]) / 2)
       );
 
-    console.log(
-      `Token count: ${totalTokenCount}\nRemaining context space: ${-thresholdOverrun}`
-    );
+    // console.log(
+    //   `Token count: ${totalTokenCount}\nRemaining context space: ${-thresholdOverrun}`
+    // );
+
+    // console.log({
+    //   compressionThreshold: this.compressionThreshold,
+    //   truncationThreshold,
+    //   thresholdOverrun,
+    //   cumulativeTokenCounts,
+    // });
 
     if (thresholdOverrun > 0) {
-      for (let i = 1; i < events.length; i++) {
-        const precedingTokens = cumulativeTokenCounts[i - 1];
-        if (precedingTokens > truncationThreshold) {
-          // const summarizedEvents = events.slice(1, i);
+      let i = 1;
+      // prettier-ignore
+      for (; i < events.length && cumulativeTokenCounts[i - 1] <= truncationThreshold; i++);
+      // prettier-ignore
+      for (; i > 0 && cumulativeTokenCounts[i - 1] > this.compressionThreshold; i--);
+      i = Math.max(i, 3);
 
-          const summaryWordLimit = Math.floor(
-            (this.compressionThreshold * AVG_WORDS_PER_TOKEN) / 6
-          );
-          const { actionText: summaryContent } = await makeDecision(
+      const precedingTokens = cumulativeTokenCounts[i - 1];
+      // console.log({ truncationIndex: i, precedingTokens });
+      const summaryWordLimit = Math.floor(
+        (this.compressionThreshold * AVG_WORDS_PER_TOKEN) / 6
+      );
+
+      const eventsToSummarize = events.slice(0, i);
+
+      // console.log(
+      //   `Summarizing ${eventsToSummarize.length} events (${eventsToSummarize
+      //     .map(countTokens)
+      //     .reduce((sum, next) => sum + next, 0)} tokens)`
+      // );
+
+      const { actionText: summaryContent } = await makeDecision(this.agentId, [
+        ...eventsToSummarize,
+        {
+          type: "message",
+          message: messageBuilder.ok(
             this.agentId,
-            [
-              ...events.slice(0, i),
-              {
-                type: "message",
-                message: messageBuilder.ok(
-                  this.agentId,
-                  `Write a summary in ${summaryWordLimit} words or less of what has happened since (but not including) the introductory message. Include key information that you learned which you don't want to forget. This information will serve as a note to yourself to help you understand what has gone before. Use the second person voice, as if you are someone filling in your replacement who knows nothing. The summarized messages will be omitted from your context window going forward and you will only have this summary to go by, so make it as useful and information-dense as possible. Be as specific as possible, but only include important information. If there are details that seem unimportant, or which you could recover outside of your memory (for instance the particular contents of a file which you could read any time), then omit them from your summary. Once again, your summary must not exceed ${summaryWordLimit} words. In this particular instance, your response should just be raw text, not formatted as an action.`
-                ),
-              },
-            ]
-          );
+            `Write a summary in ${summaryWordLimit} words or less of what has happened since (but not including) the introductory message. Include key information that you learned which you don't want to forget. This information will serve as a note to yourself to help you understand what has gone before. Use the second person voice, as if you are someone filling in your replacement who knows nothing. The summarized messages will be omitted from your context window going forward and you will only have this summary to go by, so make it as useful and information-dense as possible. Be as specific as possible, but only include important information. If there are details that seem unimportant, or which you could recover outside of your memory (for instance the particular contents of a file which you could read any time), then omit them from your summary. Once again, your summary must not exceed ${summaryWordLimit} words. In this particular instance, your response should just be raw text, not formatted as an action.`
+          ),
+        },
+      ]);
 
-          const summaryEvent: Event = {
-            type: "message",
-            message: messageBuilder.spontaneous(
-              this.agentId,
-              `Several events are omitted here to free up space in your context window, summarized as follows:\n\n${summaryContent}`
-            ),
-          };
-          const summaryTokens = countTokens(summaryEvent);
-          const tokenSavings = precedingTokens - summaryTokens;
-          if (tokenSavings > 0) {
-            // console.log(
-            //   `Summarized events, saving ${tokenSavings} tokens:\n\n${summary}`
-            // );
+      const summary = `Several events are omitted here to free up space in your context window, summarized as follows:\n\n${summaryContent}`;
+      // const summary =
+      //   "Several events are omitted here to free up space in your context window.";
+      const summaryEvent: Event = {
+        type: "message",
+        message: messageBuilder.spontaneous(this.agentId, summary),
+      };
+      const summaryTokens = countTokens(summaryEvent);
+      const tokenSavings =
+        precedingTokens - summaryTokens - cumulativeTokenCounts[0];
+      if (tokenSavings > 0) {
+        // console.log(
+        //   `Summarized events, saving ${tokenSavings} tokens:\n\n${summary}`
+        // );
 
-            return [events[0], summaryEvent, ...events.slice(i)];
-          }
-        }
+        const newEvents = [events[0], summaryEvent, ...events.slice(i)];
+
+        // const newCumulativeTokenCounts =
+        //   this.getCumulativeTokenCounts(newEvents);
+        // console.log({ newCumulativeTokenCounts });
+
+        return newEvents;
       }
     }
+
     return events;
+  }
+
+  private getCumulativeTokenCounts(events: Event[]) {
+    return events.reduce((counts, event) => {
+      const prevSum = counts.length ? counts[counts.length - 1] : 0;
+      counts.push(prevSum + countTokens(event));
+      return counts;
+    }, [] as number[]);
   }
 
   private get key() {
