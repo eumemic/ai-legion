3afe075329bed66322dbd93b8e0d466b02b3bc6d
eumemic <eumemic@gmail.com>
2023-04-10 13:57:55 -0700

    sort files by time

--- src/store/file-store.ts
+++ src/store/file-store.ts
@@ -32,7 +32,22 @@ export class FileStore implements Store {
 
   async list() {
     await this.mkdirs();
-    return (await readdir(this.dirPath)).map(decodeFilename);
+    const fileNames = await readdir(this.dirPath);
+
+    // Get file stats for each file in parallel
+    const withStats = await Promise.all(
+      fileNames.map(async (fileName) => {
+        const stats = await stat(`${this.dirPath}/${fileName}`);
+        return { keyName: decodeFilename(fileName), stats };
+      })
+    );
+
+    // Sort by last modified date
+    const sortedKeys = withStats
+      .sort((a, b) => a.stats.mtime.getTime() - b.stats.mtime.getTime())
+      .map(({ keyName }) => keyName);
+
+    return sortedKeys;
   }
 
   private async mkdirs() {
