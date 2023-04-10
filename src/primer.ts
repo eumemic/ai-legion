a574d76e2ae01f83f12a9fac72a5df4fa1b8e09b
eumemic <eumemic@gmail.com>
2023-04-10 13:57:54 -0700

    update primer with wording about invoking help before invoking an action for the first time

--- src/primer.ts
+++ src/primer.ts
@@ -52,6 +52,8 @@ ${CODE_BLOCK_DELIMITER}
 
 You may only invoke actions mentioned in the above list.
 
+*NOTE* You never invoke an action until you have first invoked \`help\` on it so that you know what parameters it expects. Being the careful agent that you are, you do not simply guess parameters that you think would make sense.
+
 3) When passing multiple lines of text as an action parameter, you *MUST* use the multi-line delimiter \`${MULTILINE_DELIMITER}\` to enclose the parameter value in its entirety.
 
 Example:
