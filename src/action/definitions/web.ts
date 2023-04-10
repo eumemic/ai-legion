346dc5448146de40bfca489a51f159a4c5524a4d
eumemic <eumemic@gmail.com>
2023-04-10 13:57:54 -0700

    add readPage action

--- src/action/definitions/web.ts
+++ src/action/definitions/web.ts
@@ -1,3 +1,4 @@
+import Mercury from "@postlight/mercury-parser";
 import { google } from "googleapis";
 import { messageBuilder } from "../../message";
 import { defineActionModule } from "../action-module";
@@ -39,6 +40,46 @@ export default defineActionModule({
             .join("\n\n")}`
         )
       );
+    },
+  },
+
+  readPage: {
+    description:
+      "Extract content from a web page while preserving hyperlink details.",
+    parameters: {
+      url: {
+        description: "The URL of the web page to read.",
+      },
+    },
+    async execute({
+      parameters: { url },
+      context: { sourceAgentId },
+      sendMessage,
+    }) {
+      try {
+        const extractedData = await Mercury.parse(url);
+
+        const result: any = extractedData;
+        if (result.error) throw Error(result.message);
+
+        sendMessage(
+          messageBuilder.ok(
+            sourceAgentId,
+            `Title: ${extractedData.title || "Unknown"}\n\nAuthor: ${
+              extractedData.author || "Unknown"
+            }\n\nPublication Date: ${
+              extractedData.date_published || "Unknown"
+            }\n\nContent:\n\n${extractedData.content}`
+          )
+        );
+      } catch (e: any) {
+        sendMessage(
+          messageBuilder.error(
+            sourceAgentId,
+            `Error extracting content from ${url}: ${e.message}`
+          )
+        );
+      }
     },
   },
 });
