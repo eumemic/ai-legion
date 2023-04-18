b74929a4091d40acd7d739b295cc8dedfa45693f
jc.durbin <jc.durbin@ardanis.com>
2023-04-18 15:03:10 +0100

    added default directories to jobs

--- dist/memory/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Memory = void 0;
-var memory_1 = require("./memory");
-Object.defineProperty(exports, "Memory", { enumerable: true, get: function () { return memory_1.Memory; } });
