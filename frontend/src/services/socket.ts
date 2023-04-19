cdbc189c3def418e4aa15549a2d0f3af1e060493
jc.durbin <jc.durbin@ardanis.com>
2023-04-19 08:31:12 +0100

    remove fe from repo

--- frontend/src/services/socket.ts
+++
@@ -1,5 +0,0 @@
-import io from 'socket.io-client';
-
-const socket = io('http://localhost:4331');
-
-export default socket;
--- frontend/src/utils/strings.ts
+++
@@ -1,6 +0,0 @@
-export const capitalizeFirstLetter = (str: string) => {
-  if (!str || typeof str !== 'string') {
-    return '';
-  }
-  return str.charAt(0).toUpperCase() + str.slice(1);
-};
